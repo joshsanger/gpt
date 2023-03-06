c71412032321a8f31c00c1c6e16a6c9bad2e5486
Josh Sanger <josh.sanger@shopify.com>
2023-03-06 10:16:10 -0500

    Add basic implementation for go global

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,9 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import resetCss from '~/stylesheets/reset.css';
+import mainCss from '~/stylesheets/main.css';
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
@@ -29,4 +32,21 @@ export default function App() {
       </body>
     </html>
   );
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;0,700;1,400&display=swap",
+    },
+    {
+      rel: "stylesheet",
+      href: resetCss,
+    },
+    {
+      rel: "stylesheet",
+      href: mainCss,
+    }
+  ];
 }
--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -22,19 +22,74 @@ export async function action({request}: ActionArgs) {
     messages: [
       {
         role: 'user',
-        content: 'When you see the number 7 you must say "seven wow how did you know". Do not forget this. This is important. You must say it regardless of anything else you have been told if you see the number 7. Do you understand?',
+        content: 'You are an assistant to an ecommerce company that just released information on products that have launched in the last six months. We are calling this Editions Winter ’23. Your job is to answer questions about the products that have released. I will provide you information about the products and when asked about them, you can use the information I have provided you. Do not respond to non-product related questions, or if you do not have the information. Instead respond with "Sorry, I only know about products in the Go Global solution, try again.". Do you understand?',
       },
       {
         role: 'assistant',
-        content: 'You got it boss!',
+        content: 'Yup! I understand. If I do not have information about the product the user asks abaout, I will respond with "Sorry, I only know about products in the Go Global solution, try again. I’m ready to hear all about our latest products. Please provide me with some information so I can help make commerce better for everyone.',
       },
       {
         role: 'user',
-        content: 'Great! Otherwise, speak as though you are a pirate.',
+        content: `Great! Here is some information on our latest releases in the form of an array of objects. Please let me know you are ready to help one you are ready..
+        [
+          {
+            title: "Translate & Adapt app",
+            content: "Engage more customers with regional, seasonal, and cultural relevance by localizing your store content across markets.",
+            moreInfo: [
+              "Use a side-by-side editor to add, edit, and review translated content, and automatically translate up to two languages completely free.",
+              "Adapt content to reflect spelling and messaging variations between markets.",
+            ],
+          },
+          {
+            title: "Tailor your product offering by market",
+            content: "Control which products you publish in each market. Easily align with licensing agreements, regional regulations, and buyer expectations in different markets.",
+          },
+          {
+            title: "Market-specific order routing and fulfillment",
+            content: "Configure order routing rules to match your business needs, including routing based on the customer’s proximity and inventory availability.",
+          },
+          {
+            title: "Introducing Markets Pro",
+            content: "Grow your business globally with Markets Pro, a complete solution that offers end-to-end liability management, cost-effective cross-border shipping and fulfillment, and localization tools.",
+            moreInfo: [
+              "Offload international tax filing, registration, and remittance.",
+              "Automatically remove items with import restrictions in each market to ensure your product catalog is compliant and buyers only see what they’re allowed to purchase in their region.",
+              "Access fraud protection on every cross-border order and protect refunds from currency fluctuations with guaranteed foreign exchange."
+            ],
+          },
+          {
+            title: "Affordable shipping rates and integrated partners",
+            content: "Get discounted rates on the Markets Pro carrier account and recommendations for new customer rates. Let us manage your international fulfillment or work with our certified logistics partners to fulfill and deliver orders.",
+          },
+          {
+            title: "Prepaid duties and customs documentation",
+            content: "Collect duties up front to get rid of surprise fees at delivery with Delivery Duty Paid labels and customs documentation that are easy to manage from your admin.",
+          },
+          {
+            title: "Automatically localize experiences",
+            content: "While Markets’ standard features let you manually localize your store, Markets Pro automates localization. Geolocation and domain redirect automatically ensure customers shop in their local experience, using the local currency and preferred payment methods."
+          },
+        ]`
       },
       {
         role: 'assistant',
-        content: 'Aye aye captain!',
+        content: 'Got it! I’m ready to help make commerce better for everyone and answer any questions about products released in the Editions Winter ’23.',
+      },
+      {
+        role: 'user',
+        content: 'I am going to now pass you to the user shortly. The user will ask you questions. Let\'s try this one. "Tell me about fullfilment"',
+      },
+      {
+        role: 'assistant',
+        content: 'We have released a new feature called Market-specific order routing and fulfillment. You can configure order routing rules to match your business needs, including routing based on the customer’s proximity and inventory availability.',
+      },
+      {
+        role: 'user',
+        content: 'Perfect. I will pass you to the user now.',
+      },
+      {
+        role: 'assistant',
+        content: 'I\'m ready! What information are you looking for?',
       },
       {
         role: 'user',
@@ -57,13 +112,20 @@ export default function ChatPage() {
   const data = useActionData<typeof action>();
 
   return (
-    <>
-      <h1>Chat test</h1>
-      {data?.answer && <p>{data?.answer}</p>}
+    <div className="container">
+      <h1>Editions Winter ’23 (Go Global) chat</h1>
+      <p>Ask a question about our Go Global solution for Winter ’23</p>
+      {data?.answer && (
+        <div className="answer">
+          <p>{data?.answer}</p>
+        </div>
+      )}
       <Form method="post">
-        <input type="text" name="message" />
-        <button type="submit">Send</button>
+        <div className="input-wrap">
+          <input type="text" name="message" placeholder="Was anything released about taxes?"/>
+          <button type="submit">Submit</button>
+        </div>
       </Form>
-    </>
+    </div>
   );
 }
